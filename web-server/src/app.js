a40196bf9da1572fde6990789493bd4bf9d1d0ac
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2024-06-22 09:24:30 +0330

    serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,27 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Hello express!</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send({
+        name: 'Amir',
+        page: 'help',
+        age: 21
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send({
+        forecast: 'It is snowing',
+        page: 'Your weather',
+        location: 'LA'
+    })
 })
 
 app.listen(3000, () => {
