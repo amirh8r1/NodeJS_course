be697c49981dd691749be45b944b3f2259feda5c
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2024-07-13 11:19:13 +0330

    async/await

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('example-id').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('example-id').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete('example-id')
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('example-id').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
